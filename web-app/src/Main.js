114408f86606e98c5035a5e56ba56d9326fe9bc9
Sunghoon Lee <sun@Sunghoons-MacBook-Pro.local>
2024-03-15 00:25:50 +0900

    improvements

--- web-app/src/Main.js
+++ web-app/src/Main.js
@@ -4,7 +4,8 @@ import restaurantImage from './asset/ll-food.jpg';
 import bruschettaImage from './asset/bruschetta.jpeg';
 import greekSaladImage from './asset/greek-salad.jpeg';
 import lemonDessertImage from './asset/dessert.jpeg';
-// import './WeekSpecials.css';
+import {faStar, faStarHalfStroke} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Body () {
     return (
@@ -30,6 +31,7 @@ const MealCard = ({ meal }) => {
       <article className="meal-card">
         <div className="meal-card-image">
           <img src={meal.image} alt={meal.name} />
+          <div className="overlay"></div>
         </div>
         <div className="meal-card-header">
           <h3>{meal.name}</h3>
@@ -63,7 +65,7 @@ const Specials = () => {
         <div className="week-specials-header">
           <h1>This week specials!</h1>
         </div>
-        {meals.map((meal, index) => 
+        {meals.map((meal, index) =>
           <MealCard key={index} meal={meal} />
         )}
       </section>
@@ -71,17 +73,82 @@ const Specials = () => {
   };
 
 
-function WeAre() {
+function OurStory() {
     return (
         <section>
             <article class="we_are">
                 <h1>About Us</h1>
-                <p>We are a family owned Mediterranean restaurant, 
+                <p>We are a family owned Mediterranean restaurant,
                     focused on traditional recipes served with a modern twist.</p>
             </article>
         </section>
     );
 }
+
+function Testimonial() {
+    return (
+        <section>
+            <article class="testimonial">
+                <h1>About Us</h1>
+                <p>We are a family owned Mediterranean restaurant,
+                    focused on traditional recipes served with a modern twist.</p>
+            </article>
+        </section>
+    );
+}
+
+
+const customers = [
+  {
+    fullName: 'Customer 1',
+    rating: [1, 1, 1, 1, 0.5],
+    says: `love the atmosphere`,
+  },
+  {
+    fullName: 'Customer 2',
+    rating: [1, 1, 1, 1, 1],
+    says: `really liked the food`,
+  },
+  {
+    fullName: 'Customer 3',
+    rating: [1, 1, 1, 1, 0.5],
+    says: `I will definitely revisit!`,
+  },
+];
+
+const ratingLevels = { '0.5': faStarHalfStroke, '1': faStar, };
+
+const TestimonialCard = ({ customer }) => {
+  return (
+    <article className="testimonial-card">
+      <h4>{customer.fullName}</h4>
+      <span>
+        {customer.rating.map((ratingPoint, index) => 
+          <FontAwesomeIcon 
+            key={index} 
+            icon={ratingLevels[ratingPoint]} 
+            size="xs" 
+          />
+        )}
+      </span>
+      <blockquote><p>"{customer.says}"</p></blockquote>
+    </article>
+  );
+};
+
+const Testimonials = () => {
+  return (
+    <section className="testimonials">
+      <div className="container grid">
+        <h1>What people say about us</h1>
+        {customers.map((customer, index) => 
+          <TestimonialCard key={index} customer={customer} />
+        )}
+     </div>
+    </section>
+  );
+};
+
 
 function Chicago() {
     return(
@@ -100,7 +167,7 @@ function Main () {
         <>
             <Body/>
             <Specials/>
-            <WeAre/>
+            <Testimonials/>
             <Chicago/>
         </>
     );
